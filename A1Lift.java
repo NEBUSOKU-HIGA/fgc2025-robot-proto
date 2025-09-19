d93bed7c445b3e78e1144aec2268beea6f07a29a
leesungze <leesungze@hotmail.com>
2025-09-19 20:39:29 +0900

    update comment3

--- A1Lift.java
+++ A1Lift.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 
 public class A1Lift {
 	
-	// リフト用モーター lift motor_._
+	// リフト用モーター lift motor_.
 	private DcMotor liftMotor;
 	
 	// ポテンショメーター制御用変数
